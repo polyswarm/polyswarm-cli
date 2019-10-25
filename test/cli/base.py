c0ce51503b8a5cad6a5449c716f97270a925c2ab
Ben Schmidt <bens@polyswarm.io>
2019-10-25 11:54:07 -0400

    test invalid inputs

--- test/cli/base.py
+++ test/cli/base.py
@@ -57,38 +57,38 @@ class BaseTestCase(TestCase):
             req['headers'] = {}
         req['headers']['Authorization'] = self.test_api_key
 
-    def _do_test(self, generator, arguments, output_format, json_path, commands, with_auth=True):
+    def _do_test(self, reqs, output_format, commands, with_auth=True):
         bad_keys = ['params', 'json']
 
         cmd = ['--api-key', self.test_api_key, '--output-format', output_format,
                '--output-file', self.test_captured_output_file]
         cmd.extend(commands)
 
-        if generator:
-            success_json = self._get_test_text_resource_content(json_path)
+        if reqs:
             with requests_mock.Mocker() as mock:
-                req = generator(*arguments)
-                if with_auth:
-                    self._add_auth(req)
-                for p in bad_keys:
-                    if p in req:
-                        del req[p]
-                mock.request(text=success_json, **req)
+                for req, result in reqs:
+                    if with_auth:
+                        self._add_auth(req)
+                    for p in bad_keys:
+                        if p in req:
+                            del req[p]
+                    mock.request(text=result, **req)
                 return self.test_runner.invoke(polyswarm, cmd)
         return self.test_runner.invoke(polyswarm, cmd)
 
-    def _do_success_test(self, generator, arguments, output_format, output_path, json_path, commands, with_auth=True):
-        expected_output = self._get_test_text_resource_content(output_path)
-        result = self._do_test(generator, arguments, output_format, json_path, commands, with_auth)
-        self.assertEqual(result.exit_code, 0, msg=traceback.format_tb(result.exc_info[2]))
-        output = self._get_file_content(self.test_captured_output_file)
-        open('/tmp/blah', 'w').write(output)
-        self.assertEqual(output, expected_output)
+    def _do_success_test(self, reqs,  output_format, output_path, commands, with_auth=True):
 
-    def _do_fail_test(self, generator, arguments, output_format, output_path, json_path, commands, with_auth=True):
-        expected_output = self._get_test_text_resource_content(output_path)
-        result = self._do_test(generator, arguments, output_format, json_path, commands, with_auth)
+        result = self._do_test(reqs, output_format, commands, with_auth)
+        self.assertEqual(result.exit_code, 0, msg=traceback.format_tb(result.exc_info[2]))
+        if output_path:
+            expected_output = self._get_test_text_resource_content(output_path)
+            output = self._get_file_content(self.test_captured_output_file)
+            self.assertEqual(output, expected_output)
+
+    def _do_fail_test(self, reqs, output_format, output_path, commands, with_auth=True):
+        result = self._do_test(reqs, output_format, commands, with_auth)
         self.assertNotEqual(result.exit_code, 0, msg=traceback.format_tb(result.exc_info[2]))
-        output = result.stdout
-        open('/tmp/blah2', 'w').write(output)
-        self.assertEqual(output, expected_output)
+        if output_path:
+            expected_output = self._get_test_text_resource_content(output_path)
+            output = result.stdout
+            self.assertEqual(output, expected_output)
--- test/cli/cli_hunt_test.py
+++ test/cli/cli_hunt_test.py
@@ -12,35 +12,29 @@ except ImportError:
 TestCase.maxDiff = None
 
 
-class SearchTest(BaseTestCase):
+class ScanTest(BaseTestCase):
 
     def __init__(self, *args, **kwargs):
-        super(SearchTest, self).__init__(*args, **kwargs)
+        super(ScanTest, self).__init__(*args, **kwargs)
         self.test_runner = CliRunner()
-        self.test_api_key = '963da5a463b0ab61fe0f96f82846490d'
-        self.bad_hash = 'lmao'
-        self.test_hash = '08666dae57ea6a8ef21cfa38cf41db395e8c39c61b1f281cb6927b2bca07fb1d'
-        self.test_query = "_exists_:lief.libraries"
+        self.bad_id = "never-gonna-run-around"
 
-    def test_search_with_hash_parameter(self):
-        commands = ['search', 'hash', self.test_hash]
-        reqs = [
-            (self.request_generator.search_metadata(MetadataQuery(self.test_query)),
-             self._get_test_text_resource_content('expected_search_success_results_hash.json')),
-        ]
-        self._do_success_test(reqs, 'json', 'expected_search_hashes_output.txt', commands)
+    def test_live_lookup_invalid(self):
+        commands = ['live', 'results', self.bad_id]
+        self._do_fail_test([], 'text', None, commands)
 
-    def test_search_with_query_parameter(self):
-        commands = ['search', 'metadata', self.test_query]
-        reqs = [
-            (self.request_generator.search_metadata(MetadataQuery(self.test_query)),
-             self._get_test_text_resource_content('expected_search_success_results.json')),
-        ]
-        self._do_success_test(reqs, 'json', 'expected_cli_search_query_output.txt', commands)
+    def test_live_delete_invalid(self):
+        commands = ['live', 'delete', self.bad_id]
+        self._do_fail_test([], 'text', None, commands)
 
-    def test_search_invalid_hash(self):
-        commands = ['search', 'hash', self.bad_hash]
-        self._do_fail_test([], 'text', 'expected_cli_search_bad_hash.txt', commands)
+    def test_historical_lookup_invalid(self):
+        commands = ['historical', 'results', self.bad_id]
+        self._do_fail_test([], 'text', None, commands)
+
+    def test_historical_delete_invalid(self):
+        commands = ['historical', 'delete', self.bad_id]
+        self._do_fail_test([], 'text', None, commands)
+
 
     """
     def test_search_with_hash_parameter(self):
