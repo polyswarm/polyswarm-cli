f98f313e7fe9c52f34ed51bef5977b931d974552
Ben Schmidt <bens@polyswarm.io>
2019-11-03 06:32:34 -0400

    fix broken packaging

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@
 # -*- coding: utf-8 -*-
 from setuptools import find_packages, setup
 
-
-def parse_requirements():
-    with open('requirements.txt', 'r') as f:
-        return [r for r in f.read().splitlines() if not r.startswith('git')]
-
-
 # The README.md will be used as the content for the PyPi package details page on the Python Package Index.
 with open('README.md', 'r') as readme:
     long_description = readme.read()
@@ -24,7 +18,11 @@ setup(
     url='https://github.com/polyswarm/polyswarm-cli',
     license='MIT',
     python_requires='>=2.7,<4',
-    install_requires=parse_requirements(),
+    install_requires=[
+        'polyswarm-api>=1.0.1',
+        'click==7.0',
+        'pytest==3.9.2'
+    ],
     include_package_data=True,
     packages=find_packages('src'),
     package_dir={'': 'src'},
