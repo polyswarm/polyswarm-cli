e59c1eb086a57107323488757875cf02011ed855
sam <samuel.neto@polyswarm.io>
2019-11-13 17:03:38 -0300

    adjusting downloads to the new api

--- src/polyswarm/download.py
+++ src/polyswarm/download.py
@@ -7,11 +7,11 @@ from . import utils
 logger = logging.getLogger(__name__)
 
 
+@click.command('download', short_help='download file(s)')
 @click.option('-r', '--hash-file', help='File of hashes, one per line.', type=click.File('r'))
 @click.option('--hash-type', help='Hash type to search [default:autodetect, sha256|sha1|md5]', default=None)
 @click.argument('hash_value', nargs=-1, callback=utils.validate_hashes)
 @click.argument('destination', nargs=1, type=click.Path(file_okay=False))
-@click.command('download', short_help='download file(s)')
 @click.pass_context
 def download(ctx, hash_file, hash_type, hash_value, destination):
     """
@@ -24,10 +24,10 @@ def download(ctx, hash_file, hash_type, hash_value, destination):
         output.local_artifact(result)
 
 
+@click.command('stream', short_help='access the polyswarm file stream')
 @click.option('-s', '--since', type=click.IntRange(1, 2880), default=1440,
               help='Request archives X minutes into the past. Default: 1440, Max: 2880')
 @click.argument('destination', nargs=1, type=click.Path(file_okay=False))
-@click.command('stream', short_help='access the polyswarm file stream')
 @click.pass_context
 def stream(ctx, since, destination):
     api = ctx.obj['api']
@@ -37,11 +37,12 @@ def stream(ctx, since, destination):
         out.local_artifact(download)
 
 
-@click.option('--hash-type', help='Hash type to search [default:autodetect, sha256|sha1|md5]', default=None)
-@click.argument('hash_value', nargs=1, callback=utils.validate_hash)
 @click.command('cat', short_help='cat artifact to stdout')
+@click.option('--hash-type', help='Hash type to search [default:autodetect, sha256|sha1|md5]', default=None)
+@click.argument('hash_value', callback=utils.validate_hash)
 @click.pass_context
 def cat(ctx, hash_type, hash_value):
     api = ctx.obj['api']
     out = click.get_binary_stream('stdout')
-    api.download_to_filehandle(hash, out)
+    hashes = utils.parse_hashes([hash_value], hash_type=hash_type, log_errors=True)
+    api.download_to_filehandle(hashes[0], out)
