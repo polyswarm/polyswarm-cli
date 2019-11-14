9f9bc670bbdf46ae9eb4fde357252abe911737b3
sam <samuel.neto@polyswarm.io>
2019-11-14 17:05:08 -0300

    Revert "release 2.0.0.dev0"
    This reverts commit bc84370d

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as readme:
 
 setup(
     name='polyswarm',
-    version='2.0.0.dev0',
+    version='1.1.1',
     description='CLI for using the PolySwarm Customer APIs',
     long_description=long_description,
     long_description_content_type='text/markdown',
--- src/polyswarm/base.py
+++ src/polyswarm/base.py
@@ -14,7 +14,7 @@ from .download import download, cat, stream
 from .search import search
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
-VERSION = '2.0.0.dev0'
+VERSION = '1.1.1'
 
 
 class ExceptionHandlingGroup(click.Group):
