943f483a47d9aaf976ba2c5231ca8f3024649a82
sam <samuel.neto@polyswarm.io>
2019-11-14 14:18:46 -0300

    testing json outputs and rescan

--- tests/scan_test.py
+++ tests/scan_test.py
@@ -22,6 +22,17 @@ class SubmissionTest(BaseTestCase):
         self.test_runner = CliRunner()
         self.test_api_key = '11111111111111111111111111111111'
 
+    def test_submission_create_json(self):
+        with patch('polyswarm_api.api.PolyswarmAPI.submit', return_value=[resources.submissions(self)[0]]), \
+             patch('polyswarm_api.api.PolyswarmAPI.lookup', return_value=iter([resources.submissions(self)[0]])):
+            result = self._run_cli(['--output-format', 'json', '-c', 'gamma',
+                                    'lookup', '74ac1097-2477-4566-951a-bf0c2716642e'])
+        self._assert_json_result(
+            result,
+            expected_output=resources.submissions(self)[0].json,
+            expected_return_code=0,
+        )
+
     def test_submission_lookup_text(self):
         with patch('polyswarm_api.api.PolyswarmAPI.lookup', return_value=iter([resources.submissions(self)[0]])), \
              patch('polyswarm_api.api.PolyswarmAPI.score', return_value=iter([resources.scores(self)[0]])):
@@ -50,6 +61,29 @@ class SubmissionTest(BaseTestCase):
              patch('polyswarm_api.api.PolyswarmAPI.score', return_value=iter([resources.scores(self)[0]])):
             result = self._run_cli(['--output-format', 'text', '-c', 'gamma',
                                     'scan', os.path.join(BASE_PATH, 'malicious')])
+        self._assert_text_result(
+            result,
+            expected_output=resources.text_submissions()[0],
+            expected_return_code=0,
+        )
+
+    def test_submission_rescan_json(self):
+        with patch('polyswarm_api.api.PolyswarmAPI.submit', return_value=[resources.submissions(self)[0]]), \
+             patch('polyswarm_api.api.PolyswarmAPI.lookup', return_value=iter([resources.submissions(self)[0]])):
+            result = self._run_cli(['--output-format', 'json', '-c', 'gamma',
+                                    'rescan', '275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f'])
+        self._assert_json_result(
+            result,
+            expected_output=resources.submissions(self)[0].json,
+            expected_return_code=0,
+        )
+
+    def test_submission_rescan_text(self):
+        with patch('polyswarm_api.api.PolyswarmAPI.rescan', return_value=[resources.submissions(self)[0]]), \
+             patch('polyswarm_api.api.PolyswarmAPI.lookup', return_value=iter([resources.submissions(self)[0]])), \
+             patch('polyswarm_api.api.PolyswarmAPI.score', return_value=iter([resources.scores(self)[0]])):
+            result = self._run_cli(['--output-format', 'text', '-c', 'gamma',
+                                    'rescan', '275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f'])
         self._assert_text_result(
             result,
             expected_output=resources.text_submissions()[0],
