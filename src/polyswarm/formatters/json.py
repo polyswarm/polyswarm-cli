ddd16f3058baa83b60ecab2ae3bb582e8f78298d
sam <samuel.neto@polyswarm.io>
2019-11-15 12:34:35 -0300

    proper way to fix name clashes

--- src/polyswarm/formatters/json.py
+++ src/polyswarm/formatters/json.py
@@ -1,4 +1,4 @@
-import json as json_
+import json
 
 from . import base
 from polyswarm_api.const import USAGE_EXCEEDED_MESSAGE
@@ -8,7 +8,7 @@ class JSONOutput(base.BaseOutput):
     name = 'json'
     @staticmethod
     def _to_json(json_data):
-        return json_.dumps(json_data, sort_keys=True)
+        return json.dumps(json_data, sort_keys=True)
 
     def artifact_instance(self, result):
         self.out.write(self._to_json(result.json) + '\n')
@@ -26,21 +26,21 @@ class JSONOutput(base.BaseOutput):
         self.out.write(self._to_json(result.json) + '\n')
 
     def local_artifact(self, artifact):
-        self.out.write(json_.dumps({'hash': artifact.artifact_name, 'path': artifact.path}, sort_keys=True)+'\n')
+        self.out.write(json.dumps({'hash': artifact.artifact_name, 'path': artifact.path}, sort_keys=True)+'\n')
 
     def invalid_rule(self, e):
-        self.out.write(json_.dumps('Malformed yara file: {}'.format(e.args[0])))
+        self.out.write(json.dumps('Malformed yara file: {}'.format(e.args[0])))
 
     @staticmethod
     def usage_exceeded():
-        return json_.dumps(USAGE_EXCEEDED_MESSAGE)
+        return json.dumps(USAGE_EXCEEDED_MESSAGE)
 
 
 class PrettyJSONOutput(base.BaseOutput):
     name = 'pretty-json'
     @staticmethod
     def _to_json(json_data):
-        return json_.dumps(json_data, indent=4, sort_keys=True)
+        return json.dumps(json_data, indent=4, sort_keys=True)
 
     def artifact_instance(self, result):
         self.out.write(self._to_json(result.json) + '\n')
@@ -58,11 +58,11 @@ class PrettyJSONOutput(base.BaseOutput):
         self.out.write(self._to_json(result.json) + '\n')
 
     def local_artifact(self, artifact):
-        self.out.write(json_.dumps({'hash': artifact.artifact_name, 'path': artifact.path}, sort_keys=True)+'\n')
+        self.out.write(json.dumps({'hash': artifact.artifact_name, 'path': artifact.path}, sort_keys=True)+'\n')
 
     def invalid_rule(self, e):
-        self.out.write(json_.dumps('Malformed yara file: {}'.format(e.args[0])))
+        self.out.write(json.dumps('Malformed yara file: {}'.format(e.args[0])))
 
     @staticmethod
     def usage_exceeded():
-        return json_.dumps(USAGE_EXCEEDED_MESSAGE)
+        return json.dumps(USAGE_EXCEEDED_MESSAGE)
