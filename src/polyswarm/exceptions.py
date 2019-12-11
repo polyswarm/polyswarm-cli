e959faa1ead765cfd281b21fab6278d83897bdef
sam <samuel.neto@polyswarm.io>
2019-12-11 20:16:11 -0300

    adding termination exceptions

--- src/polyswarm/exceptions.py
+++ src/polyswarm/exceptions.py
@@ -8,3 +8,15 @@ class PolyswarmException(Exception):
 
 class NoResultsException(PolyswarmException):
     pass
+
+
+class NotFoundException(PolyswarmException):
+    pass
+
+
+class InternalFailureException(PolyswarmException):
+    pass
+
+
+class PartialResultsException(PolyswarmException):
+    pass
