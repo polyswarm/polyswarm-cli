857e61950e2d9d16cd6a2eaa5947e44bc89c603b
sam <samuel.neto@polyswarm.io>
2019-12-26 13:09:35 -0300

    adding --nowait option to scans

--- src/polyswarm/scan.py
+++ src/polyswarm/scan.py
@@ -8,18 +8,24 @@ from . import utils
 logger = logging.getLogger(__name__)
 
 
-def submit_and_wait(api, timeout, *args, **kwargs):
+def submit_and_wait(api, timeout, nowait, *args, **kwargs):
     instance = api.submit(*args, **kwargs)
+    if nowait:
+        return instance
     return api.wait_for(instance.id, timeout=timeout)
 
 
-def rescan_and_wait(api, timeout, *args, **kwargs):
+def rescan_and_wait(api, timeout, nowait, *args, **kwargs):
     instance = api.rescan(*args, **kwargs)
+    if nowait:
+        return instance
     return api.wait_for(instance.id, timeout=timeout)
 
 
-def rescanid_and_wait(api, timeout, *args, **kwargs):
+def rescanid_and_wait(api, timeout, nowait, *args, **kwargs):
     instance = api.rescanid(*args, **kwargs)
+    if nowait:
+        return instance
     return api.wait_for(instance.id, timeout=timeout)
 
 
@@ -32,16 +38,18 @@ def scan():
 @click.option('-r', '--recursive', is_flag=True, default=False, help='Scan directories recursively')
 @click.option('-t', '--timeout', type=click.INT, default=const.DEFAULT_SCAN_TIMEOUT,
               help='How long to wait for results (default: {})'.format(const.DEFAULT_SCAN_TIMEOUT))
+@click.option('-n', '--nowait', is_flag=True,
+              help='Does not wait for the submission window to close, just create it and return right away.')
 @click.argument('path', nargs=-1, type=click.Path(exists=True))
 @click.pass_context
-def file(ctx, recursive, timeout, path):
+def file(ctx, recursive, timeout, nowait, path):
     """
     Scan files or directories via PolySwarm
     """
     api = ctx.obj['api']
     output = ctx.obj['output']
 
-    args = [(api, timeout, file) for file in utils.collect_files(path, recursive=recursive)]
+    args = [(api, timeout, nowait, file) for file in utils.collect_files(path, recursive=recursive)]
 
     for instance in utils.parallel_executor(submit_and_wait, args_list=args):
         output.artifact_instance(instance)
@@ -51,9 +59,11 @@ def file(ctx, recursive, timeout, path):
 @click.option('-r', '--url-file', help='File of URLs, one per line.', type=click.File('r'))
 @click.option('-t', '--timeout', type=click.INT, default=const.DEFAULT_SCAN_TIMEOUT,
               help='How long to wait for results (default: {})'.format(const.DEFAULT_SCAN_TIMEOUT))
+@click.option('-n', '--nowait', is_flag=True,
+              help='Does not wait for the submission window to close, just create it and return right away.')
 @click.argument('url', nargs=-1, type=click.STRING)
 @click.pass_context
-def url_(ctx, url_file, timeout, url):
+def url_(ctx, url_file, timeout, nowait, url):
     """
     Scan files or directories via PolySwarm
     """
@@ -63,7 +73,7 @@ def url_(ctx, url_file, timeout, url):
     urls = list(url)
     if url_file:
         urls.extend([u.strip() for u in url_file.readlines()])
-    args = [(api, timeout, url) for url in urls]
+    args = [(api, timeout, nowait, url) for url in urls]
     kwargs = [dict(artifact_type='url') for _ in urls]
 
     for instance in utils.parallel_executor(submit_and_wait, args_list=args, kwargs_list=kwargs):
@@ -75,15 +85,17 @@ def url_(ctx, url_file, timeout, url):
 @click.option('--hash-type', help='Hash type to search [default:autodetect, sha256|sha1|md5]', default=None)
 @click.option('-t', '--timeout', type=click.INT, default=const.DEFAULT_SCAN_TIMEOUT,
               help='How long to wait for results (default: {})'.format(const.DEFAULT_SCAN_TIMEOUT))
+@click.option('-n', '--nowait', is_flag=True,
+              help='Does not wait for the submission window to close, just create it and return right away.')
 @click.argument('hash_value', nargs=-1, callback=utils.validate_hashes)
 @click.pass_context
-def rescan(ctx, hash_file, hash_type, timeout, hash_value):
+def rescan(ctx, hash_file, hash_type, timeout, nowait, hash_value):
     """
     Rescan files with matched hashes
     """
     api = ctx.obj['api']
     output = ctx.obj['output']
-    args = [(api, timeout, h) for h in utils.parse_hashes(hash_value, hash_file=hash_file)]
+    args = [(api, timeout, nowait, h) for h in utils.parse_hashes(hash_value, hash_file=hash_file)]
 
     for instance in utils.parallel_executor(rescan_and_wait, args_list=args,
                                             kwargs_list=[{'hash_type': hash_type}]*len(args)):
@@ -93,15 +105,17 @@ def rescan(ctx, hash_file, hash_type, timeout, hash_value):
 @click.command('rescanid', short_help='Rescan by submission id')
 @click.option('-t', '--timeout', type=click.INT, default=const.DEFAULT_SCAN_TIMEOUT,
               help='How long to wait for results (default: {})'.format(const.DEFAULT_SCAN_TIMEOUT))
+@click.option('-n', '--nowait', is_flag=True,
+              help='Does not wait for the submission window to close, just create it and return right away.')
 @click.argument('submission_id', nargs=-1, callback=utils.validate_id)
 @click.pass_context
-def rescanid(ctx, timeout, submission_id):
+def rescanid(ctx, timeout, nowait, submission_id):
     """
     Rescan based on the id of a previous scan
     """
     api = ctx.obj['api']
     output = ctx.obj['output']
-    args = [(api, timeout, s) for s in submission_id]
+    args = [(api, timeout, nowait, s) for s in submission_id]
 
     for instance in utils.parallel_executor(rescanid_and_wait, args_list=args):
         output.artifact_instance(instance)
