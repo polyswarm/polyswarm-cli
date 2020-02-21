6ea423f0f5be29c4a653d4dad2509652507226ff
sam <samuel.neto@polyswarm.io>
2020-02-21 18:42:14 -0300

    using newer version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     license='MIT',
     python_requires='>=2.7,<4',
     install_requires=[
-        'polyswarm-api>=2.0.0,<3',
+        'polyswarm-api>=2.0.1,<3',
         'click~=7.0',
         'future~=0.18.2',
         'click-log~=0.3.2',
