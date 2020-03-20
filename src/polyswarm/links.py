aeddcd0ccd49fd35d38d893ef32d5835780f154f
sam <samuel.neto@polyswarm.io>
2020-03-20 13:57:20 -0300

    adding better help and docstrings

--- src/polyswarm/links.py
+++ src/polyswarm/links.py
@@ -33,10 +33,14 @@ def view(ctx, sha256):
 
 
 @link.command('list', short_help='List all links with the given tags.')
-@click.option('-t', '--tag', type=click.STRING, multiple=True)
-@click.option('-f', '--family', type=click.STRING, multiple=True)
-@click.option('-g', '--or-tag', type=click.STRING, multiple=True)
-@click.option('-y', '--or-family', type=click.STRING, multiple=True)
+@click.option('-t', '--tag', type=click.STRING, multiple=True,
+              help='A tag that must be associated with the artifact.')
+@click.option('-f', '--family', type=click.STRING, multiple=True,
+              help='A family that must be associated with the artifact.')
+@click.option('-g', '--or-tag', type=click.STRING, multiple=True,
+              help='At least one of the provided or-tags must be associated with the artifact.')
+@click.option('-y', '--or-family', type=click.STRING, multiple=True,
+              help='At least one of the provided or-families must be associated with the artifact.')
 @click.pass_context
 def list_links(ctx, tag, family, or_tag, or_family):
     api = ctx.obj['api']
