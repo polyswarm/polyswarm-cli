d68788d9342f0835a1ac1d27d4f32005806920b7
sam <samuel.neto@polyswarm.io>
2020-03-26 10:55:56 -0300

    release 2.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as readme:
 
 setup(
     name='polyswarm',
-    version='2.1.0.dev0',
+    version='2.1.0',
     description='CLI for using the PolySwarm Customer APIs',
     long_description=long_description,
     long_description_content_type='text/markdown',
