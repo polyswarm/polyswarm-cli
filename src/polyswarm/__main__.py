862b28a7ee40246b03de577b450080448e555f7a
sam <samuel.neto@polyswarm.io>
2020-06-15 13:46:46 -0300

    fixing entrypoint when running as module

--- src/polyswarm/__main__.py
+++ src/polyswarm/__main__.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
-from .base import polyswarm
+import os
+import sys
+from polyswarm.client.polyswarm import polyswarm
+
+
+def main():
+    # fixing the name of the entrypoint when the module is executed as
+    # python -m s3conf
+    # https://docs.python.org/3/library/__main__.html
+    entrypoint_name = os.path.basename(sys.argv[0])
+    if entrypoint_name == '__main__.py':
+        entrypoint_name = 'polyswarm'
+
+    polyswarm(prog_name=entrypoint_name, obj={})
+
 
 if __name__ == '__main__':
-    polyswarm(obj={})
+    main()
