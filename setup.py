621072cf6c850a7118dbf87753c1cff93874eb2e
sam <samuel.neto@polyswarm.io>
2020-07-31 15:35:10 -0300

    Bump version: 2.1.3 → 2.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as readme:
 
 setup(
     name='polyswarm',
-    version='2.1.3',
+    version='2.2.0',
     description='CLI for using the PolySwarm Customer APIs',
     long_description=long_description,
     long_description_content_type='text/markdown',
