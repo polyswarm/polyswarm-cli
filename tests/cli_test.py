7b3708f432c7fba9fb68f0c9cdaed91b569cc92d
Samuel Barbosa <samuel.neto@polyswarm.io>
2021-06-08 17:14:21 -0300

    simplifying

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,14 +1,100 @@
-from tests.utils.base_test_case import BaseTestCase, vcr
-from tests.utils import file_utils
+import shutil
+import tempfile
+import os
+import json
+import yaml
+from contextlib import contextmanager
+from unittest import TestCase
+import traceback
+
+import vcr as vcr_
+from click.testing import CliRunner
+from pkg_resources import resource_filename
+
+from polyswarm.client import polyswarm as client
+
+
+vcr = vcr_.VCR(cassette_library_dir='tests/vcr',
+               path_transformer=vcr_.VCR.ensure_suffix('.vcr'))
+
+
+@contextmanager
+def TemporaryDirectory():
+    """The day we drop python 2.7 support we can use python 3 version of this"""
+    name = tempfile.mkdtemp()
+    try:
+        yield name
+    finally:
+        shutil.rmtree(name)
+
+
+class BaseTestCase(TestCase):
+    def __init__(self, *args, **kwargs):
+        super(BaseTestCase, self).__init__(*args, **kwargs)
+        self.cli = CliRunner()
+        self.click_vcr_folder = 'tests/vcr'
+        self.click_vcr_suffix = 'click'
+        self.api_url = 'http://artifact-index-e2e:9696/v2'
+        self.api_key = '11111111111111111111111111111111'
+        self.community = 'gamma'
+        self.eicar_hash = '275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f'
+
+    def _replace(self, replace, content):
+        if replace:
+            for source, replacement in replace:
+                content = content.replace(source, replacement)
+        return content
+
+    def click_vcr(self, result, name='result', replace=None):
+        test_name = self.id().rpartition('.')[2]
+        file_name = '{}.{}'.format(test_name, self.click_vcr_suffix)
+        file_path = os.path.join(os.getcwd(), self.click_vcr_folder, file_name)
+        try:
+            with open(file_path, 'r') as f:
+                data = yaml.full_load(f)
+            entry = data.get(name)
+            if entry is None:
+                entry = self._replace(replace, result.output)
+                data[name] = entry
+                with open(file_path, 'w') as f:
+                    yaml.dump(data, f)
+        except OSError:
+            entry = self._replace(replace, result.output)
+            data = {name: entry}
+            with open(file_path, 'w') as f:
+                yaml.dump(data, f)
+        return entry
+
+    def _run_cli(self, commands):
+        commands = ['-a', self.api_key, '-u', self.api_url, '-c', self.community] + commands
+        return self.cli.invoke(client.polyswarm_cli, commands, catch_exceptions=False)
+
+    def _assert_text_result(self, result, expected_result, expected_return_code=0, replace=None):
+        current_result = self._replace(replace, result.output)
+        assert current_result == expected_result
+        self.assertEqual(expected_return_code, result.exit_code, msg=traceback.format_tb(result.exc_info[2]))
+
+    def _assert_json_result(self, results, expected_results, expected_return_code=0, replace=None):
+        current_results = self._replace(replace, results.output)
+        result_lines = current_results.splitlines()
+        expected_lines = expected_results.splitlines()
+        if len(result_lines) != len(expected_lines):
+            raise AssertionError('Number of json lines does not match')
+        self.assertEqual(expected_return_code, results.exit_code, msg=traceback.format_tb(results.exc_info[2]))
+        for result, expected_result in zip(result_lines, expected_lines):
+            result = json.loads(result)
+            expected_result = json.loads(expected_result)
+            assert result == expected_result
+
+    @staticmethod
+    def resource(filename):
+        return resource_filename('tests', filename)
 
 
 class IntegrationTest(BaseTestCase):
-    """
-        These tests mock the resource end-point, thus also sanity testing polyswarm-api.
-    """
     @vcr.use_cassette()
     def test_search_hash(self):
-        result = self._run_cli(['--output-format', 'json', 'search', 'hash', self.test_hash_value])
+        result = self._run_cli(['--output-format', 'json', 'search', 'hash', self.eicar_hash])
         self._assert_json_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
@@ -18,22 +104,19 @@ class IntegrationTest(BaseTestCase):
 
     @vcr.use_cassette()
     def test_scan_submission_lookup(self):
-        result = self._run_cli(['--output-format', 'json', '-c', 'gamma', 'lookup', '19610779111217241'])
+        result = self._run_cli(['--output-format', 'json', 'lookup', '19610779111217241'])
         self._assert_json_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
     def test_scan_submission_create(self):
-        malicious_file = self._get_test_resource_file_path('malicious')
+        malicious_file = self.resource('malicious')
         result = self._run_cli([
-            '--output-format', 'json',
-            '-c', 'gamma',
-            'scan', 'file', malicious_file,
-        ])
+            '--output-format', 'json', 'scan', 'file', malicious_file])
         self._assert_json_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
     def test_scan_submission_rescan(self):
-        result = self._run_cli(['--output-format', 'json', '-c', 'gamma', 'rescan', self.test_hash_value])
+        result = self._run_cli(['--output-format', 'json', 'rescan', self.eicar_hash])
         self._assert_json_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
@@ -49,13 +132,13 @@ class IntegrationTest(BaseTestCase):
 
     @vcr.use_cassette()
     def test_live_hunt_start(self):
-        yara_file = self._get_test_resource_file_path('eicar.yara')
+        yara_file = self.resource('eicar.yara')
         result = self._run_cli(['--output-format', 'json', 'live', 'create', yara_file])
         self._assert_json_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
     def test_live_hunt_start_with_invalid_yara_file(self):
-        broken_yara_file = self._get_test_resource_file_path('broken.yara')
+        broken_yara_file = self.resource('broken.yara')
         result = self._run_cli(['--output-format', 'json', 'live', 'create', broken_yara_file])
         self._assert_text_result(
             result,
@@ -65,7 +148,7 @@ class IntegrationTest(BaseTestCase):
 
     @vcr.use_cassette()
     def test_historical_hunt_start(self):
-        yara_file = self._get_test_resource_file_path('eicar.yara')
+        yara_file = self.resource('eicar.yara')
         result = self._run_cli(['--output-format', 'json', 'historical', 'start', yara_file])
         self._assert_json_result(result, self.click_vcr(result))
 
@@ -91,23 +174,23 @@ class IntegrationTest(BaseTestCase):
 
     @vcr.use_cassette()
     def test_download(self):
-        with file_utils.temp_dir({self.test_hash_value: self.test_eicar}) as (path, files):
+        with TemporaryDirectory() as path:
             result = self._run_cli([
-                '-u', self.api_url + '/consumer', 'download', '-d', path, self.test_hash_value])
+                '-u', self.api_url + '/consumer', 'download', '-d', path, self.eicar_hash])
             expected_result = self.click_vcr(result, replace=((path, 'temporary_folder'),))
             self._assert_text_result(result, expected_result, replace=((path, 'temporary_folder'),))
 
     @vcr.use_cassette()
     def test_download_stream(self):
-        with file_utils.temp_dir({self.test_hash_value: self.test_eicar}) as (path, files):
+        with TemporaryDirectory() as path:
             result = self._run_cli(['stream', '--since', '2880', path])
             expected_result = self.click_vcr(result, replace=((path, 'temporary_folder'),))
             self._assert_text_result(result, expected_result, replace=((path, 'temporary_folder'),))
 
     @vcr.use_cassette()
     def test_download_cat(self):
-        with file_utils.temp_dir({self.test_hash_value: self.test_eicar}) as (path, files):
-            result = self._run_cli(['-u', self.api_url + '/consumer', 'cat', self.test_hash_value])
+        with TemporaryDirectory() as path:
+            result = self._run_cli(['-u', self.api_url + '/consumer', 'cat', self.eicar_hash])
             expected_result = self.click_vcr(result, replace=((path, 'temporary_folder'),))
             self._assert_text_result(result, expected_result, replace=((path, 'temporary_folder'),))
 
@@ -142,13 +225,13 @@ class LiveHuntTest(BaseTestCase):
     @vcr.use_cassette()
     def test_live_hunt_create_json(self):
         result = self._run_cli([
-            '--output-format', 'json', 'live', 'create', self._get_test_resource_file_path('eicar.yara')])
+            '--output-format', 'json', 'live', 'create', self.resource('eicar.yara')])
         self._assert_json_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
     def test_live_hunt_create_text(self):
         result = self._run_cli([
-            '--output-format', 'text', 'live', 'create', self._get_test_resource_file_path('eicar.yara')])
+            '--output-format', 'text', 'live', 'create', self.resource('eicar.yara')])
         self._assert_text_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
@@ -176,13 +259,13 @@ class HistoricalHuntTest(BaseTestCase):
     @vcr.use_cassette()
     def test_historical_hunt_create_json(self):
         result = self._run_cli([
-            '--output-format', 'json', 'historical', 'start', self._get_test_resource_file_path('eicar.yara')])
+            '--output-format', 'json', 'historical', 'start', self.resource('eicar.yara')])
         self._assert_json_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
     def test_historical_hunt_create_text(self):
         result = self._run_cli([
-            '--output-format', 'text', 'historical', 'start', self._get_test_resource_file_path('eicar.yara')])
+            '--output-format', 'text', 'historical', 'start', self.resource('eicar.yara')])
         self._assert_text_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
@@ -212,7 +295,7 @@ class RulesetTest(BaseTestCase):
     @vcr.use_cassette()
     def test_ruleset_create_json(self):
         result = self._run_cli([
-            '--output-format', 'json', 'rules', 'create', 'test', self._get_test_resource_file_path('eicar.yara')])
+            '--output-format', 'json', 'rules', 'create', 'test', self.resource('eicar.yara')])
         self._assert_json_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
@@ -244,49 +327,49 @@ class SubmissionTest(BaseTestCase):
     @vcr.use_cassette()
     def test_submission_lookup_json(self):
         result = self._run_cli([
-            '--output-format', 'json', '-c', 'gamma', 'lookup', '82046699255546478'])
+            '--output-format', 'json', 'lookup', '82046699255546478'])
         self._assert_json_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
     def test_submission_lookup_text(self):
         result = self._run_cli([
-            '--output-format', 'text', '-c', 'gamma', 'lookup', '82046699255546478'])
+            '--output-format', 'text', 'lookup', '82046699255546478'])
         self._assert_text_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
     def test_submission_create_json(self):
         result = self._run_cli([
-            '--output-format', 'json', '-c', 'gamma', 'scan', 'file', self._get_test_resource_file_path('malicious')])
+            '--output-format', 'json', 'scan', 'file', self.resource('malicious')])
         self._assert_json_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
     def test_submission_create_text(self):
         result = self._run_cli([
-            '--output-format', 'text', '-c', 'gamma', 'scan', 'file', self._get_test_resource_file_path('malicious')])
+            '--output-format', 'text', 'scan', 'file', self.resource('malicious')])
         self._assert_text_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
     def test_submission_rescan_json(self):
         result = self._run_cli([
-            '--output-format', 'json', '-c', 'gamma', 'rescan', '275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f'])
+            '--output-format', 'json', 'rescan', self.eicar_hash])
         self._assert_json_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
     def test_submission_rescan_text(self):
         result = self._run_cli([
-            '--output-format', 'text', '-c', 'gamma', 'rescan', '275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f'])
+            '--output-format', 'text', 'rescan', self.eicar_hash])
         self._assert_text_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
     def test_submission_rescan_id_json(self):
         result = self._run_cli([
-            '--output-format', 'json', '-c', 'gamma', 'rescan-id', '82046699255546478'])
+            '--output-format', 'json', 'rescan-id', '82046699255546478'])
         self._assert_json_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
     def test_submission_rescan_id_text(self):
         result = self._run_cli([
-            '--output-format', 'text', '-c', 'gamma', 'rescan-id', '82046699255546478'])
+            '--output-format', 'text', 'rescan-id', '82046699255546478'])
         self._assert_text_result(result, self.click_vcr(result))
 
 
@@ -294,13 +377,13 @@ class SearchTest(BaseTestCase):
     @vcr.use_cassette()
     def test_search_hash_with_json_output(self):
         result = self._run_cli([
-            '--output-format', 'json', 'search', 'hash', self.test_hash_value])
+            '--output-format', 'json', 'search', 'hash', self.eicar_hash])
         self._assert_json_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
     def test_search_hash_with_text_output(self):
         result = self._run_cli([
-            '--output-format', 'text', 'search', 'hash', self.test_hash_value])
+            '--output-format', 'text', 'search', 'hash', self.eicar_hash])
         self._assert_text_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
@@ -312,13 +395,13 @@ class SearchTest(BaseTestCase):
     @vcr.use_cassette()
     def test_search_metadata_with_json_output(self):
         result = self._run_cli([
-            '--output-format', 'json', 'search', 'metadata', 'hash.sha256:' + self.test_hash_value])
+            '--output-format', 'json', 'search', 'metadata', 'hash.sha256:' + self.eicar_hash])
         self._assert_json_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
     def test_search_metadata_with_text_output(self):
         result = self._run_cli([
-            '--output-format', 'text', 'search', 'metadata', 'hash.sha256:' + self.test_hash_value])
+            '--output-format', 'text', 'search', 'metadata', 'hash.sha256:' + self.eicar_hash])
         self._assert_text_result(result, self.click_vcr(result))
 
     @vcr.use_cassette()
--- tests/utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from tests.utils.base_test_case import *
