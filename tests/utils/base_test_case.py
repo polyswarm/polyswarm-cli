c59ede7f4ab3e85e5f929d87c888b568fd93724c
Samuel Barbosa <samuel.neto@polyswarm.io>
2021-06-07 21:09:40 -0300

    more cleanups

--- tests/utils/base_test_case.py
+++ tests/utils/base_test_case.py
@@ -6,22 +6,14 @@ import yaml
 import traceback
 
 import vcr as vcr_
-from deepdiff import DeepDiff
 from click.testing import CliRunner
 from pkg_resources import resource_filename
 
 from polyswarm.client import polyswarm as client
-from polyswarm_api.api import PolyswarmAPI
 
 logger = logging.getLogger(__name__)
 
 TestCase.maxDiff = None
-
-try:
-    FileNotFoundError
-except NameError:
-    FileNotFoundError = IOError
-
 
 vcr = vcr_.VCR(cassette_library_dir='tests/vcr',
                path_transformer=vcr_.VCR.ensure_suffix('.vcr'))
@@ -35,18 +27,8 @@ class BaseTestCase(TestCase):
         self.test_runner = CliRunner()
         self.api_url = 'http://artifact-index-e2e:9696/v2'
         self.test_api_key = '11111111111111111111111111111111'
-        self.api = PolyswarmAPI(self.test_api_key, community='gamma')
         self.test_hash_value = '275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f'
         self.test_eicar = b'X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
-        self.test_s3_file_url = 'http://minio:9000/'\
-                                'testing/testing/files/27/5a/02/'\
-                                '275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f?'\
-                                'response-content-disposition=attachment%3B%20filename%3Dtesting%2F'\
-                                'files%2F27%2F5a%2F02%'\
-                                '2F275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f&'\
-                                'response-content-type=application%2Foctet-stream&'\
-                                'AWSAccessKeyId=AKIAIOSFODNN7EXAMPLE'\
-                                '&Signature=VLCdYUh8skB6cRqo7RUfGrycsKo%3D&Expires=1573768889'
 
     def _replace(self, replace, content):
         if replace:
@@ -96,22 +78,5 @@ class BaseTestCase(TestCase):
             assert result == expected_result
 
     @staticmethod
-    def _assert_json_equal(first, second):
-        diff = DeepDiff(first, second, ignore_order=True)
-        if diff:
-            raise AssertionError('Input JSON\'s differ: %s' % diff)
-
-    @staticmethod
     def _get_test_resource_file_path(filename):
-        return resource_filename('tests.resources', filename)
-
-    @staticmethod
-    def _create_response(results, **kwargs):
-        response = {'result': results, 'status': 'OK'}
-        if kwargs:
-            response['offset'] = kwargs.get('offset', None)
-            response['limit'] = kwargs.get('limit', None)
-            response['has_more'] = kwargs.get('has_more', True)
-        return json.dumps(response)
-
-
+        return resource_filename('tests', filename)
