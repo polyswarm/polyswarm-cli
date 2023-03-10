b2fc32c14f3ed7bf32757cebbdfca95352442dd0
Samuel Barbosa <samuel.neto@polyswarm.io>
2023-03-10 15:53:18 -0300

    using instance id and metadata id

--- src/polyswarm/client/sandbox.py
+++ src/polyswarm/client/sandbox.py
@@ -9,22 +9,25 @@ from polyswarm.client import utils
 
 logger = logging.getLogger(__name__)
 
+
 @click.group(short_help='Interact with the Polyswarm sandbox system.')
 def sandbox():
     pass
 
+
 @sandbox.command('submit', short_help='Submit a scanned artifact to be run through sandbox.')
-@click.argument('sha256', nargs=-1, callback=utils.validate_hashes)
+@click.argument('artifact-id', nargs=-1, callback=utils.validate_id)
 @click.pass_context
-def submit(ctx, sha256):
+def submit(ctx, artifact_id):
     """
     Submit an artifact by sha256 to the sandbox system.
     """
     api = ctx.obj['api']
     output = ctx.obj['output']
 
-    for instance in api.sandbox_file(sha256):
+    for instance in api.sandbox_instances(artifact_id):
         output.sandbox_result(instance)
+
 
 @sandbox.command('list', short_help='List the names of available sandboxes.')
 @click.pass_context
