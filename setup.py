4be4b011d4e758dfd22c361f3605f9e97ce8a44f
Michael Bradford <michael.bradford@polyswarm.io>
2023-05-31 11:34:16 -0700

    DN5987 - fix ioc search output (#133)
    * fix ioc output issue
    
    * Bump version: 3.2.0 → 3.2.1
    
    ---------
    
    Co-authored-by: admin-sbneto <64160640+admin-sbneto@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as readme:
 
 setup(
     name='polyswarm',
-    version='3.2.0',
+    version='3.2.1',
     description='CLI for using the PolySwarm Customer APIs',
     long_description=long_description,
     long_description_content_type='text/markdown',
